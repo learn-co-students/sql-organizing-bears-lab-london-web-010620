f2b9adaed8a7c39a03184c8e2e3bf0508fbd16e5
Samuel Kyprianou <samuelkyprianou@gmail.com>
2020-01-16 15:07:52 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,8 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   'SELECT COUNT(temperament) FROM bears WHERE temperament = "goofy"'
-end
+end 
+
 
 def selects_bear_that_killed_Tim
   'SELECT * FROM bears WHERE name IS NULL;'
