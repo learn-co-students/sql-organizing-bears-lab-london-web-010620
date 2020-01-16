b4323407a8aa75817557e052a4f79951da58738a
pixulainen <george.apetrei@gmail.com>
2020-01-16 16:17:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (1,'Mr. Chocolate',22,'Male','brown','aggressive',true);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (2,'Rowdy',23,'Male','black','aggressive',true);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (3,'Tabitha',22,'Female','brown','aggressive',true);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (4,'Sergeant Brown',33,'Male','brown','aggressive',false);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (5,'Melissa',22,'Female','brown','aggressive',true);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (6,'Grinch',22,'Male','brown','aggressive',false);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (7,'Wendy',16,'Female','black','aggressive',true);
+INSERT INTO bears (id, name, age, gender, color,temperament,alive) VALUES (8,null,null,'Male','brown','aggressive',true);
